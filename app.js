259d8391afd678f1bbb47633e30cd7f7e73654dc
Victor Iberi <iberivictor7@gmail.com>
2022-07-10 12:28:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const dark = document.querySelector('.dark')
 const night = document.querySelector('.night')
 const light = document.querySelector('.light')
 const sidebar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
 const activeListItem = document.querySelector('.list-item.active')
 
 dark.addEventListener('click', () => {
+    container.className = "container"
     sidebar.className = 'sidebar'
     activeListItem.className = 'list-item active'
 })
 
 night.addEventListener('click', () => {
+    container.className = "container night"
     sidebar.className = 'sidebar night'
     activeListItem.className = 'list-item active night'
 })
 
 light.addEventListener('click', () => {
+    container.className = "container light"
     sidebar.className = 'sidebar light'
     activeListItem.className = 'list-item active light'
 })
